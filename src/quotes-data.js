b73ee38596203994a604cb532b6a6221ea9bfbf1
Vincent Washington <kuraiokamiblaze@gmail.com>
2022-08-05 19:34:44 -0600

    added in all necessary functions and styling

---
+++ src/quotes-data.js
@@ -0,0 +1,127 @@
+export const quotes = [
+    {
+        id: 1,
+        quote: "History has shown us that courage can be contagious, and hope can take on a life of its own",
+        author: "Michelle Obama"
+    },
+    {
+        id: 2,
+        quote: "You can't separate peace from freedom because no one can be at peace unless he has his freedom.",
+        author: "Malcolm X"
+    },
+    {
+        id: 3,
+        quote: "If there is no struggle, there is no progress.",
+        author: "Frederick Douglass"
+    },
+    {
+        id: 4,
+        quote: "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
+        author: "Barack Obama"
+    },
+    {
+        id: 5,
+        quote: "Education is the most powerful weapon which you can use to change the world.",
+        author: "Nelson Mandela"
+    },
+    {
+        id: 6,
+        quote: "The time is always right to do what is right.",
+        author: "Martin Luther King, Jr"
+    },
+    {
+        id: 7,
+        quote: "In recognizing the humanity of our fellow beings, we pay ourselves the highest tribute.",
+        author: "Thurgood Marshall"
+    },
+    {
+        id: 8,
+        quote: "Hate is too great a burden to bear. It injures the hater more than it injures the hated.",
+        author: "Coretta Scott King"
+    },
+    {
+        id: 9,
+        quote: "If you wake up deciding what you want to give versus what you're going to get, you become a more successful person. In other words, if you want to make money, you have to help someone else make money.",
+        author: "Russell Simmons"
+    },
+    {
+        id: 10,
+        quote: "Defining myself, as opposed to being defined by others, is one of the most difficult challenges I face.",
+        author: "Carol Moseley-Braun"
+    },
+    {
+        id: 11,
+        quote: "A life is not important except in the impact it has on other lives.",
+        author: "Jackie Robinson"
+    },
+    {
+        id: 12,
+        quote: "My humanity is bound up in yours, for we can only be human together." ,
+        author: "Desmond Tutu"
+    },
+    {
+        id: 13,
+        quote: "It isn't where you come from; it's where you're going that counts." ,
+        author: "Ella Fitzgerald"
+    },
+    {
+        id: 14,
+        quote: "Character is power." ,
+        author: "Booker T. Washington"
+    },
+    {
+        id: 15,
+        quote: "I don't have a feeling of inferiority. Never had. I'm as good as anybody, but no better.",
+        author: "Katherine Johnson"
+    },
+    {
+        id: 16,
+        quote: "Hold on to your dreams of a better life and stay committed to striving to realize it.",
+        author: "Earl G. Graves, Sr."
+    },
+    {
+        id: 17,
+        quote: "You can only become accomplished at something you love. Don't make money your goal. Instead pursue the things you love doing and then do them so well that people can't take their eyes off you." ,
+        author: "Maya Angelou"
+    },
+    {
+        id: 18,
+        quote: "Have a vision. Be demanding.",
+        author: "Colin Powell"
+    },
+    {
+        id: 19,
+        quote: "Hold fast to dreams, for if dreams die, life is a broken-winged bird that cannot fly.",
+        author: "Langston Hughes"
+    },
+    {
+        id: 20,
+        quote: "There are times in life when, instead of complaining, you do something about your complaints." ,
+        author: "Rita Dove"
+    },
+    {
+        id: 21,
+        quote: "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.",
+        author: "Harriet Tubman"
+    },
+    {
+        id: 22,
+        quote: "Never be limited by other people's limited imaginations.",
+        author: "Dr. Mae Jemison"
+    },
+    {
+        id: 23,
+        quote: "Whatever we believe about ourselves and our ability comes true for us.",
+        author: "Susan L. Taylor"
+    },
+    {
+        id: 24,
+        quote: "Every time you state what you want or believe, you're the first to hear it. It's a message to both you and others about what you think is possible. Don't put a ceiling on yourself." ,
+        author: "Oprah Winfrey"
+    },
+    {
+        id: 25,
+        quote: "Education is transformational. It changes lives. That is why people work so hard to become educated and why education has always been the key to the American Dream, the force that erases arbitrary divisions of race and class and culture and unlocks every person's God-given potential.",
+        author: "Condoleezza Rice"
+    },
+];
